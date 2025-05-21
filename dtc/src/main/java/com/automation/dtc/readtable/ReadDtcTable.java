5234f4088dff8af5499cb6d382b2c75501ad8c5a
mtouhafi <mtouhafi@L-MA-5CG1362K45.corp.capgemini.com>
2025-05-21 20:55:47 +0100

    Modification of the file : Adding the following methods : binaryToHex, concatDigits and toTwoBitBinary

--- dtc/src/main/java/com/automation/dtc/readtable/ReadDtcTable.java
+++ dtc/src/main/java/com/automation/dtc/readtable/ReadDtcTable.java
@@ -35,16 +35,16 @@ public class ReadDtcTable {
 		this.rcdPath = rcdPath;
 	}
 	
-	public void readTable() {
+	public List<List<String>> readTable() {
+		List<List<String>> data_dtc = new ArrayList<>();
 		try {
 			FileInputStream file = new FileInputStream(rcdPath);
 			XSSFWorkbook workbook = new XSSFWorkbook(file);
 			XSSFSheet sheet = workbook.getSheet("LECTURE_DEFAUTS");
 
-			
 			int maxEmptyRows = 3;
 			int emptyCount = 0;
-			List<List<String>> data_dtc = new ArrayList<>();
+			
 			for (int i = startRow; i <= sheet.getLastRowNum(); i++) {
 			    Row row = sheet.getRow(i);
 			    if (row == null) {
@@ -73,6 +73,12 @@ public class ReadDtcTable {
 			    for (int col : new int[] {2, 3, 4, 6}) {
 			        Cell cell = row.getCell(col);
 			        String value = (cell != null) ? cell.toString().trim() : "";
+			        if(col==2) {
+			        	value = value.toUpperCase();
+			        	String secondDigit = toTwoBitBinary(value);
+			        	String convertedDigit = binaryToHex(concatDigits(value.charAt(0), secondDigit));
+			        	value = convertedDigit + value.substring(2);
+			        }
 			        cols_vals.add(value);
 			    }
 			    data_dtc.add(cols_vals);
@@ -84,5 +90,41 @@ public class ReadDtcTable {
 		} catch (IOException ex) {
 			ex.printStackTrace();
 		}
+		return data_dtc;
+	}
+	
+	public String toTwoBitBinary(String code) {
+		char digitChar = code.charAt(1);
+	    if (digitChar < '0' || digitChar > '3') {
+	        throw new IllegalArgumentException("Character must be between '0' and '3'");
+	    }
+
+	    int number = digitChar - '0';
+	    String binary = Integer.toBinaryString(number);
+	    return String.format("%2s", binary).replace(' ', '0');
+	}
+	
+	public String concatDigits(char first, String second) {
+		switch (first) {
+		case 'P':
+			return "00"+second;
+		case 'C':
+			return "01"+second;
+		case 'B':
+			return "10"+second;
+		case 'U':
+			return "11"+second;
+		default:
+			throw new IllegalArgumentException("Unexpected value: " + first);
+		}
+	}
+	
+	public String binaryToHex(String binary) {
+	    if (binary.length() != 4 || !binary.matches("[01]+")) {
+	        throw new IllegalArgumentException("Input must be a 4-digit binary string");
+	    }
+
+	    int decimal = Integer.parseInt(binary, 2);
+	    return Integer.toHexString(decimal).toUpperCase();
 	}
 }
