e1a4011a1a216cea392c53c9d4d66724d37fbb3a
mouadtouhafi <touhafimouad@gmail.com>
2025-06-14 00:37:05 +0100

    Creation of the file.

---
+++ dtc/src/main/java/com/automation/dtc/controllers/DtcController.java
@@ -0,0 +1,15 @@
+package com.automation.dtc.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import java.util.*;
+
+public class DtcController{
+    @FXML
+    private Pane listPane;
+    private Button selectAllBtn;         
+    private List<CheckBox> checkBoxes = new ArrayList<>();
+
+   
+}
