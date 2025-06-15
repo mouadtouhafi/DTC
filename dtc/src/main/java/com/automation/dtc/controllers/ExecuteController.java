0f7863035a89105bfb31140138d251b19b298a56
mouadtouhafi <touhafimouad@gmail.com>
2025-06-15 04:57:03 +0100

    Creating the files.

---
+++ dtc/src/main/java/com/automation/dtc/controllers/ExecuteController.java
@@ -0,0 +1,19 @@
+package com.automation.dtc.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class ExecuteController implements Initializable{
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+//    	buildDiag.create_unexisting_dtc_blocks(filesPaths.getDiag_files(), organizedData, organized_labels);
+//    	
+//    	readXMLMessaging.dtc_code_parameter_exists(filesPaths.getDsd_files());
+//		readXMLMessaging.fault_type_parameter_exists(filesPaths.getDsd_files());
+	}
+
+}
