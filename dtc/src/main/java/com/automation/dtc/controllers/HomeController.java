983d6a8e6701aefc9836c6270f70b05fdef03675
mouadtouhafi <touhafimouad@gmail.com>
2025-06-30 00:48:31 +0100

    Modyfing the view size of the settings window.

--- dtc/src/main/java/com/automation/dtc/controllers/HomeController.java
+++ dtc/src/main/java/com/automation/dtc/controllers/HomeController.java
@@ -14,14 +14,9 @@ import javafx.stage.Stage;
 
 public class HomeController {
 
-    @FXML
-    private Button about_btn;
-
-    @FXML
-    private Button settings_btn;
-
-    @FXML
-    private Button start_btn;
+    @FXML private Button about_btn;
+    @FXML private Button settings_btn;
+    @FXML private Button start_btn;
 
     @FXML
     void about_btn_clicked(ActionEvent event) {
@@ -32,11 +27,8 @@ public class HomeController {
     void settings_btn_clicked(ActionEvent event) throws IOException {
     	Parent root = FXMLLoader.load(getClass().getResource("/view/settings_view.fxml"));
     	Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-    	Scene scene = new Scene(root, 400,448);
-    	
-//    	scene.getStylesheets().add(getClass().getResource("darkMode.css").toExternalForm());
-    	
-    	
+    	Scene scene = new Scene(root, 650, 500);
+    	scene.getStylesheets().add(getClass().getResource("/styles/style.css").toExternalForm());
     	stage.setScene(scene);
     	stage.show();
     }
