c4a06fc3e73f7d6bdbb747583cbd7963d8a85991
mouadtouhafi <touhafimouad@gmail.com>
2025-07-02 00:12:53 +0100

    Adding logic when Submit or Cancel buttons are clicked.

--- dtc/src/main/java/com/automation/dtc/controllers/SettingController.java
+++ dtc/src/main/java/com/automation/dtc/controllers/SettingController.java
@@ -1,12 +1,19 @@
 package com.automation.dtc.controllers;
 
+import java.io.IOException;
+
 import com.automation.dtc.inputsdata.ReadDtcTable;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
+import javafx.stage.Stage;
 
 public class SettingController {
 
@@ -16,17 +23,28 @@ public class SettingController {
     
     @FXML
     public void initialize() {
-        spinner_1.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 100, 26));
+        spinner_1.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 100, ReadDtcTable.startRow));
     }
 
     @FXML
-    void cancel_btn_clicked(ActionEvent event) {
-    	
-    }
-
-    @FXML
-    void submit_btn_clicked(ActionEvent event) {
+    void cancel_btn_clicked(ActionEvent event) throws IOException {
     	ReadDtcTable.startRow = spinner_1.getValue();
+    	Parent root = FXMLLoader.load(getClass().getResource("/view/home_view.fxml"));
+    	Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    	Scene scene = new Scene(root, 650, 500);
+    	scene.getStylesheets().add(getClass().getResource("/styles/style.css").toExternalForm());
+    	stage.setScene(scene);
+    	stage.show();
     }
 
+    @FXML
+    void submit_btn_clicked(ActionEvent event) throws IOException {
+    	ReadDtcTable.startRow = spinner_1.getValue();
+    	Parent root = FXMLLoader.load(getClass().getResource("/view/home_view.fxml"));
+    	Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    	Scene scene = new Scene(root, 650, 500);
+    	scene.getStylesheets().add(getClass().getResource("/styles/style.css").toExternalForm());
+    	stage.setScene(scene);
+    	stage.show();
+    }
 }
