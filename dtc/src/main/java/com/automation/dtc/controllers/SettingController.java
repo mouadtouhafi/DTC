f4eaf6a13354766758611e95b007864b1b60513c
mouadtouhafi <touhafimouad@gmail.com>
2025-07-01 04:25:12 +0100

    Finalizing the class logic : assigning value to startRow field.

--- dtc/src/main/java/com/automation/dtc/controllers/SettingController.java
+++ dtc/src/main/java/com/automation/dtc/controllers/SettingController.java
@@ -1,5 +1,7 @@
 package com.automation.dtc.controllers;
 
+import com.automation.dtc.inputsdata.ReadDtcTable;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -8,19 +10,12 @@ import javafx.scene.control.SpinnerValueFactory;
 
 public class SettingController {
 
-    @FXML
-    private Button cancel_btn;
-
-    @FXML
-    private Spinner<Integer> spinner_1;
-
-
-    @FXML
-    private Button submit_btn;
+    @FXML private Button cancel_btn;
+    @FXML private Spinner<Integer> spinner_1;
+    @FXML private Button submit_btn;
     
     @FXML
     public void initialize() {
-        // Set value factory: min = 0, max = 100, initial = 10, step = 1
         spinner_1.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 100, 26));
     }
 
@@ -31,7 +26,7 @@ public class SettingController {
 
     @FXML
     void submit_btn_clicked(ActionEvent event) {
-
+    	ReadDtcTable.startRow = spinner_1.getValue();
     }
 
 }
